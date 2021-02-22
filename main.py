0ddf07df400d2d2c9497a23dcabea9bc6c54218c
Brian James Celine <brianceline@gmail.com>
2021-02-22 21:41:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+# Tuple items are enclosed in round brackets
+# Tuple is ordered
+# Tuple is immutable
+# Tuple elements do not need to be unique
+# Tuple can be of different data types
+
+
+'''
+Creating a Tuple
+'''
+
+# tuple = ()
+# tuple = ('cat', [4,3,2,1], (1,2,3))
+# tuple = 4321, 1234
+# tuple = 'hello',
+# tuple = ('hello',)
+# print(tuple)
+
+'''
+Access Tuple Elements
+'''
+
+tuple = (1234, 4321, 'hello')
+print(tuple[0])
+
+'''
+Negative Indexing
+'''
+
+
+'''
+Slicing Tuples in Python
+'''
+
+
+'''
+Changing a Tuple
+'''
+
+
+'''
+Deleting a Tuple
+'''
+
+
+'''
+Tuple Methods
+'''
