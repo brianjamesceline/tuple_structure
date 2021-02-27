79b5fb230afde0720c5eb37cbc59d8c37c59aa80
Brian James Celine <brianceline@gmail.com>
2021-02-27 12:59:23 +0000

    Additional info added

--- main.py
+++ main.py
@@ -16,33 +16,61 @@ Creating a Tuple
 # tuple = ('hello',)
 # print(tuple)
 
+
 '''
 Access Tuple Elements
 '''
 
 tuple = (1234, 4321, 'hello')
-print(tuple[0])
-
-'''
-Negative Indexing
-'''
+# print(tuple[0])
+# print(tuple[-2])
 
 
 '''
 Slicing Tuples in Python
 '''
 
+# fruits = ('orange', 'apple', 'pear', 'grapes', 'banana')
+
+# print(fruits[:1])
+# print(fruits[2:5])
+# print(fruits[:-2])
+# print(fruits[:2])
+# print(fruits[2:])
+# print(fruits[::2])
+# print(fruits[::-1])
+
 
 '''
 Changing a Tuple
 '''
 
+# fruits = ('orange', 'apple', 'pear', 'grapes', 'banana')
+# fruits[0] = 'pear'
+# Tuple cannot be changed. Totally Immutable.
 
 '''
 Deleting a Tuple
 '''
 
+# fruits = ('orange', 'apple', 'pear', 'grapes', 'banana')
+
+# del fruits[0]
+# print(fruits)
+# Item within tuple cannot be deleted or altered.
+
+# del fruits
+# print(fruits)
+# Name of Tuple can be deleted as a whole but not individual contents of tuple.
 
 '''
 Tuple Methods
 '''
+
+# print(dir(list))
+# print(dir(tuple))
+
+# fruits = ('orange', 'orange', 'apple', 'pear', 'grapes', 'banana')
+
+# print(fruits.count('orange'))
+# print(fruits.index('apple'))
